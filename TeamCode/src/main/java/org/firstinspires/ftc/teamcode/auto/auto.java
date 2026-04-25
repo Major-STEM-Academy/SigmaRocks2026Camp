d6c1f786f7bd4f9e55b37839d14acb890e3f3ea8
Mario <52135@students.ausd.net>
2026-04-24 19:41:43 -0700

    Opmode 8 code new auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/auto.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.FLAPPER_2_CLOSE;
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.FLAPPER_2_OPEN;
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.FLAPPER_3_CLOSE;
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.FLAPPER_3_OPEN;
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.GATE_CLOSE;
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.GATE_OPEN;
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.INTAKE_POWER_INTAKE;
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.INTAKE_POWER_OUTTAKE;
+import static org.firstinspires.ftc.teamcode.hardware.robotHardware.INTAKE_POWER_STOP;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.hardware.robotHardware;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.hardware.robotHardware;
+import org.firstinspires.ftc.teamcode.opmode.Opmode8;
+import org.firstinspires.ftc.teamcode.opmode.Opmode8.LAUNCH_STATES;
+
+import java.util.function.Supplier;
+
+@TeleOp(name = "auto")
+public class auto extends LinearOpMode {
+    private boolean bLaunchReqeusted = true;
+    ElapsedTime TimeFromSpinUp = new ElapsedTime();
+    public LAUNCH_STATES LaunchState= LAUNCH_STATES.IDLE;
+    public enum LAUNCH_STATES {
+        IDLE,
+        SPIN_UP,
+        LAUNCH,
+        LAUNCHED,   // trigger back to READY
+    }
+    public robotHardware robot = new robotHardware();
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+        waitForStart();
+
+        robot.setDrivePower(-0.3, -0.3, -0.3, -0.3);
+        sleep(2000);
+        robot.setDrivePower(0, 0, 0, 0);
+        TimeFromSpinUp.reset();
+        while(bLaunchReqeusted)
+            launch();
+        robot.setDrivePower(1.0,-1.0,-1.0,1.0);
+        sleep(1000);
+        robot.setDrivePower(0, 0, 0, 0);
+    }
+    void launch(){
+
+        if (!bLaunchReqeusted)
+            return;
+        switch(LaunchState){
+            case IDLE:
+                TimeFromSpinUp.reset();
+                robot.pusher.setPosition(0.5);
+                robot.motorshoot.setVelocity(2000);
+                LaunchState= LAUNCH_STATES.SPIN_UP;
+                telemetry.addData("case","IDLE");
+                break;
+            case SPIN_UP:
+                if(TimeFromSpinUp.seconds()>2.5)
+                    LaunchState= LAUNCH_STATES.LAUNCH;
+
+                telemetry.addData("motor speed", robot.motorshoot.getVelocity());
+                break;
+            case LAUNCH:
+                robot.pusher.setPosition(0);
+                if(TimeFromSpinUp.seconds()<3) break;
+                robot.pusher.setPosition(1);
+                robot.motorshoot.setVelocity(0);
+                telemetry.addData("case","LAUNCH");
+                LaunchState= LAUNCH_STATES.LAUNCHED;
+                break;
+            case LAUNCHED:
+                robot.motorshoot.setVelocity(0);
+                robot.pusher.setPosition(1);
+
+                LaunchState = LAUNCH_STATES.IDLE;
+                bLaunchReqeusted = false;
+                break;
+        }
+    }
+    
+}
